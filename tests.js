64f3a6d024f146e0e790a921d66d1bd9f589817e
Irina Perkhurova <irinaperkh@gmail.com>
2018-01-14 16:31:41 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 var index = require("./index.js")
+var height = "70"
 
 if (index.name === "Susan") {
     console.log("The name is correct")
@@ -8,7 +9,7 @@ if (index.name === "Susan") {
 
 
 
-if (index.height === 70) {
+if (height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
